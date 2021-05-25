fd2a21f6a4d8f175460c6241d87caa802cafbe78
JMercie <themercie@gmail.com>
2021-05-25 20:54:11 -0300

    feat(ChildProcesses): :construction: improve structure of the code
    Implement a new Response class in order to standarize the return of functions to easier handle in the UI.

--- scripts/git/init.js
+++ scripts/git/init.js
@@ -1,48 +1,75 @@
 import os from 'os';
 import { exec, execSync } from 'child_process';
-import { dirname } from 'path';
+import * as path from 'path';
+
+/**
+ * @description -> This class is used to wrap response data to identify wether or not the function succeed.
+ * defaults to unsuccessful operation.
+ */
+class Response {
+  constructor(code = -1, status = 'Error', error = true) {
+    this.code = code;
+    this.status = status;
+    this.error = error;
+  }
+}
 
 /**
  * @description -> This function is use to create a directory in the users home and initialize a
  * git repository on it plus create a sfdx project from scratch.
  * @param {String} name -> Name of the directory the user is creating.
  * @param {String} result ->  success message.
+ * @returns {Response} res -> instance of response object with info about outcome of this function.
  */
 export const initNewRepo = async (projectName, directory) => {
-  try {
-    const targetDir = directory ? directory : os.homedir();
+  const res = new Response();
+  let succeed;
+  directory ??= os.homedir();
 
-    exec(`sfdx force:project:create -t standard -x -n ${projectName}`, {
-      cwd: targetDir,
-      shell: true
-    });
+  const sfdx_projectCreate = exec(
+    `sfdx force:project:create -t standard -x -n ${projectName}`,
+    {
+      cwd: directory,
+      shell: true // this is to spawn .bat or .cmd when running in windows.
+    }
+  );
+  sfdx_projectCreate.on('exit', (code) => {
+    if (code === 0) succeed = true;
+  });
 
-    exec(
-      'git init',
-      {
-        cwd: `${targetDir}/${projectName}`,
-        shell: true
-      },
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${stderr}`);
-          return;
-        }
-        return stdout;
-      }
-    );
+  const git_init = exec('git init', {
+    cwd: `${directory}/${projectName}`,
+    shell: true // this is to spawn .bat or .cmd when running in windows.
+  });
+  git_init.on('exit', (code) => {
+    if (code === 0) succeed = true;
 
-    return `successfully create git repository at path: ${directory}/${projectName}`;
-  } catch (error) {
-    console.error(error);
+    succeed = false;
+  });
+
+  // TODO: operation succeed but is not getting into this branch
+  if (succeed) {
+    res.code = 0;
+    res.status = 'Success';
+    res.error = null;
   }
+
+  return res;
 };
+
+initNewRepo('test', '/Users/jmercie/dev/github/')
+  .then((res) => {
+    console.log({ res });
+  })
+  .catch((err) => {
+    console.error({ err });
+  });
 
 /**
  * @description This method look recursively around a given path (default is user home directory) and return a list with
  * the paths that meet the criteria.
  * @param {String} name -> name of the directory where you want to find git repositories.
- * @returns {String[]} output -> list of paths that fullfil search criteria
+ * @returns {Response} res -> instance of response object with info about outcome of this function.
  */
 export const openExistingSFDXProject = async (name) => {
   name ??= os.homedir();
@@ -72,8 +99,8 @@ export const openExistingSFDXProject = async (name) => {
 };
 
 /**
- * @description cloneRepo handle the succesfull clone of a git remote repository. Currently just work for public repositories.
- * @return {Boolean} Returns true if operation succeed, false otherwise.
+ * @description cloneRepo handle the successful clone of a git remote repository. Currently just work for public repositories.
+ * @returns {Response} res -> instance of response object with info about outcome of this function.
  */
 export const cloneRepo = async (repoName, directory) => {
   execSync(
@@ -92,3 +119,5 @@ export const cloneRepo = async (repoName, directory) => {
 
   return true;
 };
+
+//TODO make a response handler to be more DRY.
