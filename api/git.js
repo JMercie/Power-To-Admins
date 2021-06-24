f31120d223a63794a8f3ac4c318b5e2c867e0da2
JMercie <themercie@gmail.com>
2021-06-24 19:40:53 -0300

    refactor(ChildProcesses): :recycle: Refactor sfdx child process and git handlers
    Now git-helper.js contains two handlers: one for success and one for failure operations. Remove logic of sfdx task from git.js and adjust all git methods to use the new handlers from git-helper

--- api/git.js
+++ api/git.js
@@ -1,20 +1,23 @@
 'use strict';
 import os from 'os';
 import * as path from 'path';
-import { execSync } from 'child_process';
-import { handleSuccess } from './git-helper.js';
 import simpleGit from 'simple-git';
+import { handleError, handleSuccess } from './git-helper.js';
+import {sf_newProject} from './sfdx.js'
 
 /**
  * @author JMercie
  * @date 2021-05-20
  * @description this objects is used to configure how the simple-git task will run and perform.
- * @LastModifiedDate 2021-06-20
+ * @LastModifiedDate 2021-06-24
  */
 const options = {
   baseDir: process.cwd(),
   binary: 'git',
-  maxConcurrentProcesses: 6
+  maxConcurrentProcesses: 6,
+  timeout: {
+    block: 2000
+  }
 };
 
 /**
@@ -31,90 +34,25 @@ const git = simpleGit(options);
  * @date 2021-05-20
  * @description -> This function is use to create a directory in the users home and initialize a
  * git repository on it plus create a sfdx project from scratch.
- * @param {String} name -> Name of the directory the user is creating.
- * @param {String} result ->  success message.
- * @returns {Response} res -> instance of response object with info about outcome of this function.
- * @LastModifiedDate 2021-06-20.
+ * @param {String} projectName -> Name of the project the user is creating.
+ * @param {String} directory -> Name of the directory the user is creating.
+ * @returns {Result || Error}  Stdout or Stderr of any of the child process being executed.
+ * @LastModifiedDate 2021-06-24.
  */
-// !Needs refactor
-export const initNewRepo = async (projectName, directory) => {
+export const init = async (projectName = 'awesome sfdx project', directory) => {
   directory ??= os.homedir();
-
-  execSync(
-    `sfdx force:project:create -t standard -x -n ${projectName}`,
-    {
-      cwd: directory,
-      shell: true,
-      stdio: 'inherit'
-    },
-    (error, _stdout, stderr) => {
-      if (error) {
-        console.error(error);
-        return;
+  await sf_newProject(projectName, directory).catch(err =>  err);
+  
+  options.baseDir = path.join(directory, projectName);
+  const {data, error} = await git.checkIsRepo()
+    .then( isRepo => {
+      if (!isRepo) {
+       return git.init(options, handleSuccess)
       }
-
-      if (stderr) {
-        console.error(stderr);
-      }
-    }
-  );
-
-  execSync(
-    'git init',
-    {
-      cwd: `${directory}/${projectName}`,
-      shell: true,
-      stdio: 'inherit'
-    },
-    (error, _stdout, stderr) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      if (stderr) {
-        console.error(stderr);
-      }
-    }
-  );
-};
-
-/**
- * @author JMercie
- * @date 2021-05-20
- * @description This method look recursively around a given path (default is user home directory) and return a list with
- * the paths that meet the criteria.
- * @param {String} name -> name of the directory where you want to find git repositories.
- * @returns {Response} res -> instance of response object with info about outcome of this function.
- * @LastModifiedDate 2021-06-20
- */
-// !Needs refactor
-export const openExistingSFDXProject = async (name) => {
-  name ??= os.homedir();
-  let output = [];
-
-  // here we look for folders that contains a file with name: sfdx-project.json. Because is mandatory for every local sfdx project.
-  const matches = execSync(
-    `find ${name} -name "sfdx-project.json"`,
-    (error, _stdout, stderr) => {
-      if (error || stderr) {
-        console.log(`exec error: ${error}`);
-        console.log(stderr);
-        return;
-      }
-    }
-  )
-    .toString()
-    .split('\n');
-  matches.pop(); // this will remove the last array item, which is a empty string.
-
-  matches.forEach((data) => {
-    output.push(path.posix.dirname(data));
-  });
-
-  return output.length > 0
-    ? output
-    : 'there is no SFDX projects in your machine, try create a new one!';
+      return;
+    }).catch(handleError);
+  
+    return data ?? error;
 };
 
 /**
@@ -123,50 +61,37 @@ export const openExistingSFDXProject = async (name) => {
  * @description cloneRepo handle the successful clone of a git remote repository. Currently just work for public repositories.
  * @param {String} repoName -> Remote repository name that the user wants to clone.
  * @param {String} directory -> Target directory where we can clone the repository.
- * @returns {Response} res -> instance of response object with info about outcome of this function.
- * @LastModifiedDate 2021-06-20
+ * @returns {Result || GitError}  stdout in case of success or GitError in case of failure.
+ * @LastModifiedDate 2021-06-24
  */
-// !Needs refactor
 export const cloneRepo = async (repoName, directory) => {
-  execSync(
-    `git clone ${repoName}`,
-    {
-      cwd: directory ? directory : os.homedir(),
-      shell: true
-    },
-    (error, _stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return false;
-      }
-
-      if (stderr) console.error(stderr);
-    }
-  );
-
-  return true;
+  if (!repoName) {
+    return;
+  }
+  options.baseDir = directory || os.homedir();
+  const {data, error} =  await git.clone(repoName, options,handleSuccess).catch(handleError);
+  return data ?? error;
 };
 
 /**
  * @author JMercie
  * @date 2021-06-20
- * @description addToRepo  -> method is used to stage the desire files in order to commit them.
- * @param {String[]} files -> Is an array of files that we want to stage, if empty all tracked changes are being stage
- * @param {String} workingDir -> Working directory where we perform this operation.
+ * @description addToRepo  Is used to stage the desire files in order to commit them.
+ * @param {String[]} files  Is an array of files that we want to stage, if empty all tracked changes are being stage
+ * @param {String} directory  Working directory where we perform this operation.
  *  This value must be provided by the UI where its controlled in which repository we're working.
- * @returns Data or Error -> String with info about the running process.
- * @LastModifiedDate 2021-06-20
+ * @returns {Result || GitError}  String with info about the running process.
+ * @LastModifiedDate 2021-06-24
  */
-export const addToRepo = async (files, workingDir) => {
-  files ??= './*';
-  options.baseDir = workingDir ? workingDir : '';
-  if (!options.baseDir) {
+export const addToRepo = async (files = './*', directory) => {
+  if (!directory) {
     return;
   }
-
-  // We don't return data here because in a successful operation the simpleGit.add() method returns undefined.
-  const { _data, error } = await handleSuccess(git.add(files));
-  if (error) return error;
+  options.baseDir = directory;
+  const { data, error } = await git
+    .add(files, handleSuccess)
+    .catch(handleError);
+  return data ?? error;
 };
 
 /**
@@ -174,12 +99,15 @@ export const addToRepo = async (files, workingDir) => {
  * @date 2021-06-20
  * @description commit -> This will commit al staged files in the current working directory.
  * @param {String} message -> Commit message that the user choose for his commit.
- * @returns Data or Error -> String with info about the running process
- * @LastModifiedDate 2021-06-20
+ * @returns {Result || GitError} -> String with info about the running process
+ * @LastModifiedDate 2021-06-24
  * */
-export const commit = async (message) => {
-  message ??= 'Commit message provided by Power-To-Admins';
-  const { data, error } = await handleSuccess(git.commit(message));
+export const commit = async (
+  message = 'Commit message provided by Power-To-Admins'
+) => {
+  const { data, error } = await git
+    .commit(message, handleSuccess)
+    .catch(handleError);
   return data ?? error;
 };
 
@@ -189,15 +117,15 @@ export const commit = async (message) => {
  * @description This function allow to add a remote Repository.
  * @param {String} remoteName='origin' -> Name of the remote repository
  * @param {String} remoteRepo -> URL of the remote repository.
- * @returns {String || Buffer} -> String with info about the running process.
- * @LastModifiedDate 2021-06-21.
+ * @returns {Result || GitError} -> String with info about the running process.
+ * @LastModifiedDate 2021-06-24.
  */
 export const addRemote = async (remoteName = 'origin', remoteRepo) => {
   if (remoteRepo) return 'No remote URL was provided.';
 
-  const { data, error } = await handleSuccess(
-    git.addRemote(remoteName, remoteRepo)
-  );
+  const { data, error } = await git
+    .addRemote(remoteName, remoteRepo, handleSuccess)
+    .catch(handleError);
   return data ?? error;
 };
 
@@ -207,11 +135,13 @@ export const addRemote = async (remoteName = 'origin', remoteRepo) => {
  * @description This allow os to push to a remote repository our local changes.
  * @param {String} remoteName='origin' -> Name of the remote repository where we are pushing changes.
  * @param {String} branch -> Name of the branch in the remote repository.
- * @returns {String || Buffer} -> Returns stdout when successful and a Buffer concatenation between stdout and stderr if fails.
- * @LastModifiedDate 2021-06-21.
+ * @returns {Result || GitError} -> Returns stdout when successful or GitError if fails.
+ * @LastModifiedDate 2021-06-24.
  */
 export const push = async (remoteName = 'origin', branch) => {
-  const { data, error } = await handleSuccess(git.push(remoteName, branch));
+  const { data, error } = await git
+    .push(remoteName, branch, handleSuccess)
+    .catch(handleError);
   return data ?? error;
 };
 
@@ -219,10 +149,12 @@ export const push = async (remoteName = 'origin', branch) => {
  * @author JMercie
  * @date 2021-06-21
  * @description This function allow us to fetch changes of the current remote repository.
- * @returns {Error || Null}
- * @LastModifiedDate 2021-06-21.
+ * @returns {Result || GitError} Returns stdout when successful or GitError if fails.
+ * @LastModifiedDate 2021-06-24.
  */
 export const fetch = async () => {
-  const { error } = await handleSuccess(git.fetch());
-  return error ?? null;
+  const { data, error } = await git.fetch(handleSuccess).catch(handleError);
+  return data ?? error;
 };
+
+export const pull = async () => {};
