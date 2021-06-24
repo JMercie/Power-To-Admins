3b399f0439c8fd8cb01e746a540b3d70835a31d9
JMercie <themercie@gmail.com>
2021-06-24 19:40:53 -0300

    refactor(ChildProcesses): :recycle: Refactor sfdx child process and git handlers
    Now git-helper.js contains two handlers: one for success and one for failure operations. Remove logic of sfdx task from git.js and adjust all git methods to use the new handlers from git-helper

--- api/git-helper.js
+++ api/git-helper.js
@@ -3,15 +3,27 @@
 /**
  * @author JMercie
  * @date 2021-06-20
- * @description handleSuccess -> This functions responsibility is to avoid many try/catch blocks per use in promises.
- * @param {Promise} operation -> Running task for the git binary that we're executing at the moment.
- * @returns {Array} -> Returns data (stdout) in case of success an stderr in case of failure
- * @LastModifiedDate 2021-06-21
+ * @description handleSuccess -> Trailing callback function to handle success of git tasks.
+ * @param {Error} err -> fatal error of the current task being run by the git binary.
+ * @param {Result} taskResult -> result (stdout) of the current task. Can be a string, a stream or undefined.
+ * @returns {[Result || GitError]} -> Returns data (stdout) in case of success and stderr in case of failure
+ * @LastModifiedDate 2021-06-24
  */
-export const handleSuccess = async (operation) => {
-  const data = await operation.catch (error => {
-    console.error({error});
-    return [null, error];
-  });
-  return [data, null];
+export const handleSuccess = async (err, taskResult) => {
+  if (err.message || err.git) {
+    return [null, err.message || String(err.git)];
+  }
+  return [taskResult, null];
+};
+
+/**
+ * @author JMercie
+ * @date 2021-06-24
+ * @description handleError -> To be called inside the catch() of the git promise.
+ * @param {GitError} gitError -> fatal error returned by the git task.
+ * @returns {GitError}
+ * @LastModifiedDate 2021-06-24
+ */
+export const handleError = async (gitError) => {
+  return gitError;
 };
